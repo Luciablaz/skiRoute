cc2a4c698843cb25bc3fca642a705fdfa829da1a
Luciablaz <luciablazquezgallardo@gmail.com>
2026-04-20 12:11:54 +0200

    Datos de Cerler subidos

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 DB_CONFIG = {
     "dbname":   os.getenv("DB_NAME",     "skiRoute"),
     "user":     os.getenv("DB_USER",     "postgres"),
-    "password": os.getenv("DB_PASSWORD", ""),
+    "password": os.getenv("DB_PASSWORD", "1234asdf"),
     "host":     os.getenv("DB_HOST",     "localhost"),
     "port":     os.getenv("DB_PORT",     "5432"),
 }
